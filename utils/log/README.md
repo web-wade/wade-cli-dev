6528074f1cbc9e8dafa7433dde4c0fe397ebdff6
wade <756059388@qq.com>
2021-05-02 15:27:31 +0800

    添加node版本判断

---
+++ utils/log/README.md
@@ -0,0 +1,11 @@
+# `@wade-cli-dev/log`
+
+> TODO: description
+
+## Usage
+
+```
+const log = require('@wade-cli-dev/log');
+
+// TODO: DEMONSTRATE API
+```
---
+++ utils/log/lib/index.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const log = require("npmlog")
+
+log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info"
+log.heading = 'wade'
+log.addLevel("success", 2000, { fg: "green" })
+
+module.exports = log
