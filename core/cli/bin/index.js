a042a5e248960cecd4822b03ec6ba7765dea876b
wade <756059388@qq.com>
2022-03-25 17:29:02 +0800

    add

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -4,7 +4,7 @@ const importLocal = require("import-local")
 
 
 if(importLocal(__filename)){
-  require('npmlog').info('cli','正在使用本地版本')
+  require('npmlog').info('cli','正在使用本地版本!')
 }else{
   require('../lib')(process.argv.slice(2))
 }
